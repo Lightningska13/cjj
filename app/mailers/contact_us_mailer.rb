808e3e51d933f815bdf4b469ba78761d3145ebea
Patrick <patrick.lighthall@gmail.com>
2013-01-03 13:44:24 -0500

    Delete mail.rb File

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,10 +1,10 @@
 class ContactUsMailer < ActionMailer::Base
 
-  default :to => "janssen.catherine@gmail.com", :subject => "Contact From Website"
+  default :to => "janssen.catherine@gmail.com", :subject => "Contact From Website", :from => "website@cathyjojanssen.com"
 
   def send_confirmation(message)
     @message = message
-    mail(:from => @message.email ) do |format|
+    mail(:reply_to => @message.email ) do |format|
       format.text
     end
   end
